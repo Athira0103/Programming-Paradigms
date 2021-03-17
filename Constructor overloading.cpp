d02dfa335b6c1fd1f57a835cc0596e9e2ecdab82
Athira0103 <54762671+Athira0103@users.noreply.github.com>
2021-03-17 19:58:30 +0530

    Assignment done
    Sample program for constructor overloading

---
+++ Constructor overloading.cpp
@@ -0,0 +1,68 @@
+//Assignment 7  - C++ Program using Class  to perform addition of two complex numbers using the concept of constructor overloading and friend function.
+#include<iostream>
+#include<math.h>
+using namespace std;
+class complex
+{	
+	int real,img;
+	public:
+	friend void add(complex c1,complex c2);
+	complex()
+	{
+	}	
+	
+	complex(int r)
+	{
+		real = r;
+		img = r;
+	}
+	
+	complex(int real,int img)
+	{
+		real = real;
+		img = img;
+	}
+	
+	complex(complex &c)
+	{
+		real = c.real;
+		img = c.img;
+	}
+	
+	void print()
+	{
+		cout<<real;
+		if(img>=0)
+		cout<<" + "<<img<<"i"<<endl;
+		else
+		cout<<" - "<<abs(img)<<"i"<<endl;
+	}
+		
+};
+void add(complex c1,complex c2)
+{
+	complex c4;
+	c4.real = c1.real+c2.real;
+	c4.img = c1.img+c2.img;
+	cout<<"The sum is : ";
+	c4.print();	
+}
+
+main()
+{
+	int n,m,o,p;	
+	cout<<"Enter the real and imaginary values of 1st complex number. "<<endl;
+	cin>>n>>m;
+	complex c1(n,m);
+	cout<<"Enter the real and imaginary values of 2nd complex number. "<<endl;
+	cin>>o>>p;
+	complex c2(o,p);
+	cout<<"First number : ";
+	c1.print();
+	cout<<"Second number : ";
+	c2.print();	
+	complex c3(c1);
+	cout<<"Third number : ";
+	c3.print();
+	add(c2,c3);	
+}
